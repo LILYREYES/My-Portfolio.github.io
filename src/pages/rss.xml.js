410e3966a2964b28ad6c11cb098785d765f6e37a
LILYREYES <lilia.lnci@hotmail.com>
2025-10-20 21:48:09 -0600

    Update RSS feed title and description

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,8 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: 'Web Development and Technology Blog | Fernando López | EFEELE',
-    description: 'Welcome to my blog, where I share my passion for frontend development, web design, and the latest technology trends.',
+    title: 'Web Development and Design Blog | Myriam Lilia Reyes |',
+    description: 'Welcome to my blog, where I share my passion for frontend development, web design, and the latest things that I did.',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>es</language>`,
